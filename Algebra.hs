9503208dd0948d1625d68c08b375c59eec6208f5
Anthony Burzillo <burz@jhu.edu>
2014-05-16 15:51:40 -0400

    Add MAlgebra and mcata

--- Algebra.hs
+++ Algebra.hs
@@ -1,17 +1,23 @@
 module Algebra
 ( Algebra
+, MAlgebra
 , Fix(..)
 , unFix
 , cata
+, mcata
 ) where
 
 type Algebra f a = f a -> a
+type MAlgebra m f a = f (m a) -> m a
 
 newtype Fix f = Fx (f (Fix f))
 
 unFix :: Fix f -> f (Fix f)
 unFix (Fx x) = x
 
-cata :: Functor f => (f a -> a) -> Fix f -> a
+cata :: Functor f => Algebra f a -> Fix f -> a
 cata alg = alg . fmap (cata alg) . unFix
+
+mcata :: Functor f => MAlgebra m f a -> Fix f -> m a
+mcata alg = alg . fmap (cata alg) . unFix
 

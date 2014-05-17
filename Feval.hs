3712e7b29df467c8dfab42419277b06ecefae8da
Anthony Burzillo <burz@jhu.edu>
2014-05-17 05:39:45 -0400

    Add useful definitions to Feval

--- Feval.hs
+++ Feval.hs
@@ -1,5 +1,9 @@
+{-# LANGUAGE FlexibleInstances #-}
+
 module Feval
-( Result(..)
+( run_eval
+, run_typecheck
+, Result(..)
 , run
 ) where
 
@@ -10,12 +14,21 @@ import Algebra
 import Type
 import Eval
 
+instance Show (Fix Expr) where
+    show = show . evalTransform
+
+run_eval :: Fix Expr -> Maybe RVal
+run_eval = eval . evalTransform
+
+run_typecheck :: Fix Expr -> Maybe FType
+run_typecheck = typecheck . typeTransform
+
 data Result = Result (RVal, FType) | TypeMismatch | InconsistentTypes deriving Show
 
 run :: Fix Expr -> Result
-run e = case typecheck (typeTransform e) of
+run e = case run_typecheck e of
     Nothing -> InconsistentTypes
-    Just t -> case eval (evalTransform e) of
+    Just t -> case run_eval e of
         Nothing -> TypeMismatch
         Just v -> Result (v, t)
 
